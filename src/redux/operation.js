8af0521635ed94c0a5ef4020afbad18adc8427ee
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-11-18 16:46:30 +0200

    18.11/2

--- src/redux/operation.js
+++ src/redux/operation.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-axios.defaults.baseURL = 'https://6556090d84b36e3a431ee093.mockapi.io/contacts/';
+axios.defaults.baseURL = 'https://6556090d84b36e3a431ee093.mockapi.io/contacts';
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
